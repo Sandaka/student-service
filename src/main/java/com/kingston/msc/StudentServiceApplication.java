cf15dff33a2ab5a50134a041fcc6252bfcf0537d
Sandaka <sandaka94@gmail.com>
2021-10-18 00:43:52 +0530

    first commit

---
+++ src/main/java/com/kingston/msc/StudentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.kingston.msc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class StudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceApplication.class, args);
+	}
+
+}
